b5c59e5d7fdebe41305591a9edf3c1ef8e47a42e
Charlie Lake <clake13@students.cs.ubc.ca>
2024-11-06 13:19:03 -0800

    Adding placeholder login_info.py file

---
+++ login_info_PH.py
@@ -0,0 +1,5 @@
+# the below are variables that store Piazza email/password for the user's piazza account
+email = "placeholder_email@gmail.com"
+password = "placeholder_password" 
+
+piazza_course = "Enter in the character string at the end of a given course's Piazza URL e.g., https://piazza.com/class/abcdefg123, the course code is abcdefg123"
